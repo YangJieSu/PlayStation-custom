50c9fe7b6512a8ccedad31c7986d11f3ebbf2333
CloudSu <BetaPhoenixSNSD@gmail.com>
2019-02-03 03:58:02 +0800

    調整logo路徑

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/PlayStation-custom/dist',
+    assetsPublicPath: '/PlayStation-custom/dist/',
 
     /**
      * Source Maps
