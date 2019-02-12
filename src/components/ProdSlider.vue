764ebf979115bbc27ce87eadb1b328547ccf558f
CloudSu <BetaPhoenixSNSD@gmail.com>
2019-02-12 14:06:10 +0800

    修正 swiper

--- src/components/ProdSlider.vue
+++ src/components/ProdSlider.vue
@@ -53,6 +53,41 @@ export default {
         if(response.data.success) {
           vm.products = response.data.products;
           vm.isLoading = false;
+          vm.showSwiper();
+        }
+      });
+    },
+    showSwiper() {
+      var mySwiper = new Swiper("#prodSwiper", {
+        slidesPerView: 4,
+        slidesPerGroup: 4,
+        spaceBetween: 10,
+        grabCursor: true,
+        speed: 600,
+        autoplay: {
+          delay: 5000,
+        },
+        observer : true ,  //启动动态检查器(OB/观众/观看者)，当改变swiper的样式（例如隐藏/显示）或者修改swiper的子元素时，自动初始化swiper。
+        breakpoints: {
+          480: {
+            slidesPerView: 1,
+            slidesPerGroup:1,
+            spaceBetween: 20,      
+          },
+          767: {
+            slidesPerView: 2,
+            slidesPerGroup:2,
+            spaceBetween: 30
+          },
+          992: {
+            slidesPerView: 3,
+            slidesPerGroup:3,
+            spaceBetween: 30
+          }
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true
         }
       });
     }
@@ -73,46 +108,10 @@ export default {
       };
     }
   },
-  mounted() {
-    var mySwiper = new Swiper("#prodSwiper", {
-      // Optional parameters
-      // direction: "horizontal",
-      slidesPerView: 4,
-      slidesPerGroup: 4,
-      spaceBetween: 10,
-      grabCursor: true,
-      speed: 600,
-      autoplay: {
-        delay: 5000,
-      },
-      breakpoints: {
-        480: {
-          slidesPerView: 1,
-          slidesPerGroup:1,
-          spaceBetween: 20,      
-        },
-        767: {
-          slidesPerView: 2,
-          slidesPerGroup:2,
-          spaceBetween: 30
-        },
-        992: {
-          slidesPerView: 3,
-          slidesPerGroup:3,
-          spaceBetween: 30
-        }
-      },
-      // If we need pagination
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true
-      }
-    });
-  },
   created() {
     const vm = this;
     vm.getProducts();
-  },
+  }
 }
 </script>
 
